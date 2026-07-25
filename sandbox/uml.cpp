f1c4ab8b8ea21d585681e9d21c0a60303755bb71
Neil Aldea <njaldea@gmail.com>
2026-07-26 01:54:59 +0800

    [nil-sm] v0.0.1

---
+++ sandbox/uml.cpp
@@ -0,0 +1,335 @@
+#include <nil/sm/uml.hpp>
+
+#include <iostream>
+
+namespace sandbox::uml_example
+{
+    struct ev_submit_order
+    {
+    };
+
+    struct ev_validation_ok
+    {
+    };
+
+    struct ev_validation_failed
+    {
+    };
+
+    struct ev_payment_ok
+    {
+    };
+
+    struct ev_payment_failed
+    {
+    };
+
+    struct ev_pick_done
+    {
+    };
+
+    struct ev_pack_done
+    {
+    };
+
+    struct ev_ship
+    {
+    };
+
+    struct ev_delivered
+    {
+    };
+
+    struct ev_notify_customer
+    {
+    };
+
+    struct ev_notification_sent
+    {
+    };
+
+    struct ev_status_ping
+    {
+    };
+
+    struct ev_escalate
+    {
+    };
+
+    struct ev_cancel
+    {
+    };
+
+    struct draft_order;
+    struct validating_order;
+    struct awaiting_payment;
+    struct paid_order;
+
+    struct draft_order
+    {
+        using events = nil::xalt::tlist<ev_submit_order, ev_cancel>;
+
+        static auto on_enter()
+        {
+            return nil::sm::NOOP();
+        }
+
+        static auto on_event(const ev_submit_order& /* event */)
+        {
+            return nil::sm::Transit<validating_order>{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct validating_order
+    {
+        using events = nil::xalt::tlist<ev_validation_ok, ev_validation_failed, ev_cancel>;
+
+        static auto on_event(const ev_validation_ok& /* event */)
+        {
+            return nil::sm::Transit<awaiting_payment>{};
+        }
+
+        static auto on_event(const ev_validation_failed& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct awaiting_payment
+    {
+        using events = nil::xalt::tlist<ev_payment_ok, ev_payment_failed, ev_cancel>;
+
+        static auto on_event(const ev_payment_ok& /* event */)
+        {
+            return nil::sm::Transit<paid_order>{};
+        }
+
+        static auto on_event(const ev_payment_failed& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct paid_order
+    {
+        using events = nil::xalt::tlist<ev_cancel>;
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct order_lifecycle
+    {
+        using regions = nil::xalt::tlist<draft_order>;
+        using events = nil::xalt::tlist<ev_cancel>;
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct picking;
+    struct packing;
+    struct ready_to_ship;
+
+    struct picking
+    {
+        using events = nil::xalt::tlist<ev_pick_done, ev_cancel>;
+
+        static auto on_event(const ev_pick_done& /* event */)
+        {
+            return nil::sm::Transit<packing>{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct packing
+    {
+        using events = nil::xalt::tlist<ev_pack_done, ev_cancel>;
+
+        static auto on_event(const ev_pack_done& /* event */)
+        {
+            return nil::sm::Transit<ready_to_ship>{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct ready_to_ship
+    {
+        using events = nil::xalt::tlist<ev_ship, ev_status_ping, ev_cancel>;
+
+        static auto on_event(const ev_ship& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+
+        static auto on_event(const ev_status_ping& /* event */)
+        {
+            return nil::sm::Discard{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct waiting_for_courier;
+    struct in_transit;
+    struct delivered;
+
+    struct waiting_for_courier
+    {
+        using events = nil::xalt::tlist<ev_ship, ev_cancel>;
+
+        static auto on_event(const ev_ship& /* event */)
+        {
+            return nil::sm::Transit<in_transit>{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct in_transit
+    {
+        using events = nil::xalt::tlist<ev_delivered, ev_cancel>;
+
+        static auto on_event(const ev_delivered& /* event */)
+        {
+            return nil::sm::Transit<delivered>{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct delivered
+    {
+        using events = nil::xalt::tlist<ev_cancel>;
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct fulfillment_lifecycle
+    {
+        using regions = nil::xalt::tlist<picking, waiting_for_courier>;
+        using events = nil::xalt::tlist<ev_cancel>;
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct notify_pending;
+    struct notify_sent;
+
+    struct notify_pending
+    {
+        using events = nil::xalt::tlist<ev_notify_customer, ev_escalate, ev_cancel>;
+
+        static auto on_event(const ev_notify_customer& /* event */)
+        {
+            return nil::sm::Transit<notify_sent>{};
+        }
+
+        static auto on_event(const ev_escalate& /* event */)
+        {
+            return nil::sm::Forward{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct notify_sent
+    {
+        using events = nil::xalt::tlist<ev_notification_sent, ev_cancel>;
+
+        static auto on_event(const ev_notification_sent& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct notifications
+    {
+        using regions = nil::xalt::tlist<notify_pending>;
+        using events = nil::xalt::tlist<ev_escalate, ev_cancel>;
+
+        static auto on_event(const ev_escalate& /* event */)
+        {
+            return nil::sm::Discard{};
+        }
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+
+    struct order_workflow
+    {
+        // Three orthogonal lanes running in parallel.
+        using regions = nil::xalt::tlist<order_lifecycle, fulfillment_lifecycle, notifications>;
+        using events = nil::xalt::tlist<ev_cancel>;
+
+        static auto on_event(const ev_cancel& /* event */)
+        {
+            return nil::sm::Terminate{};
+        }
+
+        static auto on_regions_finalized()
+        {
+            return nil::sm::Terminate{};
+        }
+    };
+}
+
+int main()
+{
+    nil::sm::DefaultSM<sandbox::uml_example::order_workflow> sm{nullptr, nullptr};
+    nil::sm::uml::operator<<(std::cout, sm);
+    return 0;
+}
---
+++ src/test/21_transit_dispatch_state_type_checks.cpp
@@ -0,0 +1,136 @@
+#include <nil/sm.hpp>
+
+#include <gtest/gtest.h>
+
+#include <type_traits>
+
+namespace
+{
+    template <typename Initial>
+    using dispatch_for = nil::sm::detail::region_state_factory<
+        nil::sm::default_api<Initial>,
+        typename nil::sm::detail::region_reachability_graph<nil::sm::default_api, Initial>::states>;
+
+    template <typename Dispatch, typename Target>
+    consteval bool dispatch_contains_target_id()
+    {
+        return Dispatch::targets::template any_of<std::is_same, Target>;
+    }
+
+    struct event
+    {
+    };
+
+    // Case 1: A only
+    struct only_a
+    {
+        using events = nil::xalt::tlist<event>;
+
+        static auto on_event(const event& /* event */)
+        {
+            return nil::sm::Discard{};
+        }
+    };
+
+    using dispatch_only_a = dispatch_for<only_a>;
+
+    static_assert(std::is_same_v<dispatch_only_a::targets, nil::xalt::tlist<only_a>>);
+    static_assert(dispatch_only_a::targets::size == 1);
+    static_assert(dispatch_contains_target_id<dispatch_only_a, only_a>());
+
+    // Case 2: A -> B
+    struct ab_b;
+
+    struct ab_a
+    {
+        using events = nil::xalt::tlist<event>;
+
+        static auto on_event(const event& /* event */)
+        {
+            return nil::sm::Transit<ab_b>{};
+        }
+    };
+
+    struct ab_b
+    {
+        using events = nil::xalt::tlist<event>;
+
+        static auto on_event(const event& /* event */)
+        {
+            return nil::sm::Discard{};
+        }
+    };
+
+    using dispatch_ab_a = dispatch_for<ab_a>;
+    using dispatch_ab_b = dispatch_for<ab_b>;
+
+    static_assert(std::is_same_v<dispatch_ab_a::targets, nil::xalt::tlist<ab_a, ab_b>>);
+    static_assert(dispatch_ab_a::targets::size == 2);
+    static_assert(dispatch_contains_target_id<dispatch_ab_a, ab_a>());
+    static_assert(dispatch_contains_target_id<dispatch_ab_a, ab_b>());
+    static_assert(std::is_same_v<dispatch_ab_b::targets, nil::xalt::tlist<ab_b>>);
+    static_assert(dispatch_contains_target_id<dispatch_ab_b, ab_b>());
+
+    // Case 3: A -> B -> C -> A
+    struct cycle_b;
+    struct cycle_c;
+
+    struct cycle_a
+    {
+        using events = nil::xalt::tlist<event>;
+
+        static auto on_event(const event& /* event */)
+        {
+            return nil::sm::Transit<cycle_b>{};
+        }
+    };
+
+    struct cycle_b
+    {
+        using events = nil::xalt::tlist<event>;
+
+        static auto on_event(const event& /* event */)
+        {
+            return nil::sm::Transit<cycle_c>{};
+        }
+    };
+
+    struct cycle_c
+    {
+        using events = nil::xalt::tlist<event>;
+
+        static auto on_event(const event& /* event */)
+        {
+            return nil::sm::Transit<cycle_a>{};
+        }
+    };
+
+    using dispatch_cycle_a = dispatch_for<cycle_a>;
+    using dispatch_cycle_b = dispatch_for<cycle_b>;
+    using dispatch_cycle_c = dispatch_for<cycle_c>;
+
+    static_assert(std::is_same_v<
+                  dispatch_cycle_a::targets,
+                  nil::xalt::tlist<cycle_a, cycle_b, cycle_c>>);
+    static_assert(std::is_same_v<
+                  dispatch_cycle_b::targets,
+                  nil::xalt::tlist<cycle_b, cycle_c, cycle_a>>);
+    static_assert(std::is_same_v<
+                  dispatch_cycle_c::targets,
+                  nil::xalt::tlist<cycle_c, cycle_a, cycle_b>>);
+
+    static_assert(dispatch_contains_target_id<dispatch_cycle_a, cycle_a>());
+    static_assert(dispatch_contains_target_id<dispatch_cycle_a, cycle_b>());
+    static_assert(dispatch_contains_target_id<dispatch_cycle_a, cycle_c>());
+    static_assert(dispatch_contains_target_id<dispatch_cycle_b, cycle_a>());
+    static_assert(dispatch_contains_target_id<dispatch_cycle_b, cycle_c>());
+    static_assert(dispatch_contains_target_id<dispatch_cycle_b, cycle_b>());
+    static_assert(dispatch_contains_target_id<dispatch_cycle_c, cycle_a>());
+    static_assert(dispatch_contains_target_id<dispatch_cycle_c, cycle_b>());
+    static_assert(dispatch_contains_target_id<dispatch_cycle_c, cycle_c>());
+}
+
+TEST(sm_feature_transit_dispatch_state, type_checks_compile)
+{
+    SUCCEED();
+}
